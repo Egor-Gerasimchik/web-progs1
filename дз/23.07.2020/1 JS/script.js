865100be1d00dfcaaff9164902359d8aa0c64b4a
Egor-Gerasimchik <egor.gerasimchik2007@gmail.com>
2020-08-22 14:53:28 +0700

    Homework the  22 August

---
+++ дз/23.07.2020/1 JS/script.js
@@ -0,0 +1,55 @@
+let divr = document.createElement("div");
+divr.style.height = "100px";
+divr.style.width = "100px";
+divr.style.backgroundColor = "red";
+divr.style.borderRadius = "100%";
+divr.style.marginTop = "3%";
+console.log(divr);
+
+let divb = document.createElement("div");
+divb.style.height = "100px";
+divb.style.width = "100px";
+divb.style.backgroundColor = "blue";
+divb.style.borderRadius = "100%";
+divb.style.marginTop = "3%";
+console.log(divb);
+
+let divy = document.createElement("div");
+divy.style.height = "100px";
+divy.style.width = "100px";
+divy.style.backgroundColor = "yellow";
+divy.style.borderRadius = "100%";
+divy.style.marginTop = "3%";
+console.log(divy);
+
+let divg = document.createElement("div");
+divg.style.height = "100px";
+divg.style.width = "100px";
+divg.style.backgroundColor = "green";
+divg.style.borderRadius = "100%";
+divg.style.marginTop = "3%";
+console.log(divg);
+
+let divbl = document.createElement("div");
+divbl.style.height = "100px";
+divbl.style.width = "100px";
+divbl.style.backgroundColor = "black";
+divbl.style.borderRadius = "100%";
+divbl.style.marginTop = "3%";
+console.log(divbl);
+
+let divw = document.createElement("div");
+divw.style.height = "100px";
+divw.style.width = "100px";
+divw.style.backgroundColor = "white";
+divw.style.borderRadius = "100%";
+divw.style.marginTop = "3%";
+divw.style.border = "2px solid black";
+console.log(divw);
+
+document.body.appendChild(divw);
+document.body.appendChild(divbl);
+document.body.appendChild(divg);
+document.body.appendChild(divy);
+document.body.appendChild(divb);
+document.body.appendChild(divr);
